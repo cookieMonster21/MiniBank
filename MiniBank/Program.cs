412fdb1d809ad39b622ba8d57f0c52d88ea389c1
lisa <ulbrichlisa@live.de>
2017-09-07 10:59:10 +0200

    start project

---
+++ MiniBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
